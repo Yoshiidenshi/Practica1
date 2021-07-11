ff6ae64d3c9ee929a286263f3022e51f8bb19993
Евгений Бажин <76097199+Yoshiidenshi@users.noreply.github.com>
2021-07-11 22:42:43 +0300

    Слайдшоу и ToDo

--- todolist/app.js
+++ todolist/app.js
@@ -1,44 +1,75 @@
-let buttonEnter = document.getElementById('enter');
-let userInput = document.getElementById('userInput');
-let ul = document.querySelector('ul');
+$(function() {
+    let buttonEnter = $('#enter');
+    let userInput = $('#userInput');
+    $('.intro').delay(500).animate({'opacity':'1'},200)
+    $('#first').delay(1000).animate({'opacity':'1'},500)
+    $('#second').delay(2000).animate({'opacity':'1'},500)
+    $('#third').delay(3000).animate({'opacity':'1'},500)
+    $('#enter').delay(4000).animate({'opacity':'1'},500)
+    $('input').delay(4000).animate({'opacity':'1'},500)
 
-function inputLength() {
-    return userInput.value.length > 0;
-}
+    
+    
+    let ul = $('ul');
+    let localStorage = window.localStorage;
+    let itemMap = [{
+       ind:1,
+       text:'sample'
+    }]
 
-function createTodo() {
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(userInput.value));
-    ul.appendChild(li);
-    userInput.value = '';
-
-
-    li.onclick = function() {
-        li.classList.toggle('done');
+    function inputLength() {
+        return !!userInput.val();
     }
 
-    let deleteButton = document.createElement('button');
-    deleteButton.appendChild(document.createTextNode('X'));
-    li.appendChild(deleteButton);
-    deleteButton.addEventListener('click', deleteTodoItem);
+    function createTodo() {
+        let li = $("<li>");
+        li.fadeIn(500);
+        li.append(document.createTextNode(userInput.val()));
+        ul.append(li);
 
-    function deleteTodoItem() {
-        li.classList.add('delete');
+        itemMap.push({
+            ind:itemMap.length+1,
+            text:userInput.val()
+        })
+        localStorage.setItem('Todo_items', JSON.stringify(itemMap));
+
+        userInput.val('');
+        li.click(todoDone);
+
+        let deleteButton = $('<button>');
+        deleteButton.append(document.createTextNode('X'));
+        li.append(deleteButton);
+        deleteButton.click(deleteTodoItem);
+
+        function deletecolor() {
+            li.toggleClass('deletecolor')
+        }
+
+        function deleteTodoItem() {
+            li.click(todoDone);
+            li.click(deletecolor);
+            li.animate({
+                'margin-left':'290px',
+                'margin-right':'290px',
+            },{duration:700,queue:false}).fadeOut(1000);
+        }
+
+        function todoDone() {
+            li.toggleClass('done')
+        }
     }
-}
 
-function changeListAfterKeypress(event) {
-    if ( inputLength() && event.which === 13) {
-        createTodo();
+    function changeListAfterKeypress(event) {
+        if ( inputLength() && event.which === 13) {
+            createTodo();
+        }
     }
-}
 
-userInput.addEventListener('keypress', changeListAfterKeypress);
+    userInput.keypress(changeListAfterKeypress);
 
-buttonEnter.onclick = function(){
-    if (inputLength()) {
-        createTodo();
-    }
-}
-
-
+    buttonEnter.click(function(){
+        if (inputLength()) {
+            createTodo();
+        }
+    });
+})
